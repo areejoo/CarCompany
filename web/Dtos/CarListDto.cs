52842517685f2d938e7766a093ac767a6b4ddcc5
areej <areej.fayyad96@gmail.com>
2023-08-07 10:41:58 +0200

    implement pagination

--- web/Dtos/CarListDto.cs
+++ web/Dtos/CarListDto.cs
@@ -4,28 +4,9 @@ namespace web.api.Dtos
 {
     public class CarListDto
     {
-        [Required]
-        public Guid Id { get; set; }
-        [Required]
-        public int Number { get; set; }
 
+        public ICollection<CarDto> CarsPaginationList { get; set; }
 
-        public double EngineCapacity { get; set; }
-
-        public string Color { get; set; }
-
-
-        public string Type { get; set; }
-
-
-        public double DailyFare { get; set; }
-
-
-        public bool WithDriver { get; set; }
-
-        public Guid? DriverId { get; set; }
-        
-        [Required]
         public int Count { get; set; }
     }
 }
